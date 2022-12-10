2ad9d376c76365b7aeef19012e344d1fe37c4c83
Denis Vasilev <lyon_den@mail.ru>
2022-12-11 00:50:38 +0300

    work with logic
    ColorizedViewContoroller:
    Add old logic (lesson 2.04)

---
+++ ColorizedAppV2/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  ColorizedAppV2
+//
+//  Created by Денис Васильев on 10.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
